e3ecb119013b523f75b54735b438b26f783bbb20
royfreund <royfreund14@gmail.com>
2025-03-23 03:46:08 +0200

    Now the welcome screen is the main one

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,6 @@ dependencies {
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.50")
-    implementation(libs.androidx.activity)
     kapt("com.google.dagger:hilt-android-compiler:2.50")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
 
--- app/src/main/java/com/final_project/poop_bags/WelcomeActivity.kt
+++ app/src/main/java/com/final_project/poop_bags/WelcomeActivity.kt
@@ -11,7 +11,7 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.welcome_screen)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
